c4d9754d1d5dfef95f5399bd1a762c594da0fd5d
mertoz41 <mertozka41@gmail.com>
2020-10-10 00:02:53 -0400

    lets hope

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,17 @@ import {Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 export default (props) => (
   <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="home">Art Stone Marble & Granite</Navbar.Brand>
+  <Navbar.Brand href="/home">Art Stone Marble & Granite</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="about">About</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
       <NavDropdown title="Catalog" id="basic-nav-dropdown">
-        <NavDropdown.Item href="stones">Stones</NavDropdown.Item>
+        <NavDropdown.Item href="/stones">Stones</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="sinks">Sinks</NavDropdown.Item>
+        <NavDropdown.Item href="/sinks">Sinks</NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link href="contact">Contact</Nav.Link>
+      <Nav.Link href="/contact">Contact</Nav.Link>
     </Nav>
     
   </Navbar.Collapse>
