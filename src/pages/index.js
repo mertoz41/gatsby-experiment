aedc1baba72879f244485e4f0aa209f609e26db2
Mert Ozkaynak <wwwert@Merts-MacBook-Pro.local>
2021-02-11 19:18:58 -0500

    added helmet to artstone

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,16 @@ import Footer from '../components/footer'
 import PictureSlider from '../components/pictureslider'
 import homeStyles from './home.module.css'
 import 'semantic-ui-css/semantic.min.css'
+import { Helmet } from 'react-helmet'
 
 
 const IndexPage = () => (
   <div className={homeStyles.wrapper}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>The Art Stone</title>
+          {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+        </Helmet>
         <Header />
         <PictureSlider />
         <Footer /> 
